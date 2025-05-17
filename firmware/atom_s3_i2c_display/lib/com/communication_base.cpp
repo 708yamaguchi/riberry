b93db841c44a68047d4b035a677adee442639ce6
Naoya Yamaguchi <708yamaguchi@gmail.com>
2025-05-17 22:59:59 +0900

    Add BaseCorrectionMode and its packet type

--- firmware/atom_s3_i2c_display/lib/com/communication_base.cpp
+++ firmware/atom_s3_i2c_display/lib/com/communication_base.cpp
@@ -187,6 +187,10 @@ void CommunicationBase::processPacket(const String& str, int offset) {
             instance->lcd.color_str = str.substring(offset);  // remove PacketType Header
             break;
 
+        case BASE_CORRECTION_MODE:
+            instance->lcd.color_str = str.substring(offset);  // remove PacketType Header
+            break;
+
         case BUTTON_STATE_REQUEST:
             requestEvent();
             break;
--- riberry/mode_type.py
+++ riberry/mode_type.py
@@ -17,6 +17,7 @@ class ModeType(Enum):
     WIFI_SETTINGS = 0x12
     DATA_COLLECTION = 0x13
     LEADER_FOLLOWER = 0x14
+    BASE_CORRECTION = 0x15
 
     FIRMWARE_UPDATE_MODE = 0xFF
 
@@ -37,6 +38,7 @@ MODE_TYPE_MAPPING = {
     ModeType.WIFI_SETTINGS: "WiFiSettingsMode",
     ModeType.DATA_COLLECTION: "DataCollectionMode",
     ModeType.LEADER_FOLLOWER: "LeaderFollowerMode",
+    ModeType.BASE_CORRECTION: "BaseCorrectionMode",
 
     ModeType.FIRMWARE_UPDATE_MODE: "FirmwareUpdateMode",
 }
