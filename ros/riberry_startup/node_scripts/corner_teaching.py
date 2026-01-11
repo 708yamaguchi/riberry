e4febdd1e90c735037184eb365ba6e8e8379b93e
Naoya Yamaguchi <708yamaguchi@gmail.com>
2026-01-11 23:53:20 +0900

    Update

--- ros/riberry_startup/node_scripts/corner_teaching.py
+++ ros/riberry_startup/node_scripts/corner_teaching.py
@@ -125,7 +125,8 @@ def generate_trace_trajectory(corners, corner_avs, step_width=0.02, lift_vector=
     # ロボット手先(EndEffector)座標系でのストローク方向を指定
     # "x+", "x-", "y+", "y-", "z+", "z-" が指定可能
     # target_local_axis = "y+"
-    target_local_axis = "y-"
+    # target_local_axis = "y-"
+    target_local_axis = "z-"
     # ============================================================
 
     # 1. データの展開
@@ -631,14 +632,14 @@ class CornerTeachingTask:
             "min_time_step": 0.3,           # [s]
             "pos_error_tolerance": 0.02,        # [m] IK許容誤差
             "rot_error_tolerance": np.deg2rad(5.0), # [rad] IK回転許容誤差 (rthre)
-            "gravity_comp_offset": 0.07,    # [m] Vision認識時の重力補正高さ
+            "gravity_comp_offset": 0.03,   # [m] Vision認識時の重力補正高さ
             "lift_height": 0.1,            # [m] 移動時の持ち上げ高さ
             "stir_depth": 0.06,             # [m] かき混ぜ時の深さ
             "press_stroke": 0.18,           # 押し込み深さ (基準高さより下)
             "vision_base_offset": (0.0, 0.0, 0.0),  # [m] ベース座標系相対でのオフセット
 
             # "ee_offset": (-0.1, 0.0, 0.2),  # 刷毛把持用
-            "ee_offset": (-0.12, 0.0, 0.08),  # 糊用グリッパ
+            # "ee_offset": (-0.12, 0.0, 0.08),  # 糊用グリッパ
             # "ee_offset": (0.0, 0.0, 0.08),  # デフォルトグリッパ
             # "ee_offset": (-0.03, 0.0, 0.08),  # 布巾を持つとき（カメラから離した場所が先端になる）
             # "ee_offset": (-0.12, 0.0, 0.25),  # 箸をもつとき
@@ -646,6 +647,7 @@ class CornerTeachingTask:
             # "ee_offset": (-0.12, 0.0, 0.15),    # 毛玉とるとる用エンドエフェクタ
             # "ee_offset": (-0.10, 0.0, 0.17),    # エチケットブラシ用エンドエフェクタ
             # "ee_offset": (-0.135, 0.0, 0.08),    # アイロン用エンドエフェクタ
+            "ee_offset": (-0.06, 0.0, 0.14),    # アイロン用エンドエフェクタ
 
             "min_coverage_ratio": 0.4,      # 実行を許可する最低カバー率 (40%)
             "max_seed_count": 5,            # 保存するSeedの最大数
