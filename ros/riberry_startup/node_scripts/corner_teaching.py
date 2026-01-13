ffc1bf41f19f0cab6d8a35d5fd1b26ac529ec379
Naoya Yamaguchi <708yamaguchi@gmail.com>
2026-01-13 17:44:23 +0900

    Update

--- ros/riberry_startup/node_scripts/corner_teaching.py
+++ ros/riberry_startup/node_scripts/corner_teaching.py
@@ -633,23 +633,24 @@ class CornerTeachingTask:
             "min_time_step": 0.3,           # [s]
             "pos_error_tolerance": 0.02,        # [m] IK許容誤差
             "rot_error_tolerance": np.deg2rad(5.0), # [rad] IK回転許容誤差 (rthre)
-            "gravity_comp_offset": 0.13,   # [m] Vision認識時の重力補正高さ
+            "gravity_comp_offset": 0.05,   # [m] Vision認識時の重力補正高さ
             "lift_height": 0.1,            # [m] 移動時の持ち上げ高さ
             "stir_depth": 0.06,             # [m] かき混ぜ時の深さ
             "press_stroke": 0.18,           # 押し込み深さ (基準高さより下)
+            # "vision_base_offset": (0.12, 0.0, -0.10),  # [m] ベース座標系相対でのオフセット
             "vision_base_offset": (0.0, 0.0, 0.0),  # [m] ベース座標系相対でのオフセット
 
             # "ee_offset": (-0.1, 0.0, 0.2),  # 刷毛把持用
             # "ee_offset": (-0.12, 0.0, 0.08),  # 糊用グリッパ
             # "ee_offset": (0.0, 0.0, 0.08),  # デフォルトグリッパ
-            # "ee_offset": (-0.03, 0.0, 0.08),  # 布巾を持つとき（カメラから離した場所が先端になる）
+            "ee_offset": (-0.03, 0.0, 0.08),  # 布巾を持つとき（カメラから離した場所が先端になる）
             # "ee_offset": (-0.12, 0.0, 0.25),  # 箸をもつとき
             # "ee_offset": (-0.11, 0.0, 0.14),  # 押し洗い用エンドエフェクタ
             # "ee_offset": (-0.12, 0.0, 0.15),    # 毛玉とるとる用エンドエフェクタ
             # "ee_offset": (-0.10, 0.0, 0.17),    # エチケットブラシ用エンドエフェクタ
             # "ee_offset": (-0.135, 0.0, 0.08),    # アイロン用エンドエフェクタ
             # "ee_offset": (-0.06, 0.0, 0.14),    # コロコロ用エンドエフェクタ
-            "ee_offset": (-0.05, 0.0, 0.25),    # ドライヤー用エンドエフェクタ
+            # "ee_offset": (-0.05, 0.0, 0.25),    # ドライヤー用エンドエフェクタ
 
             "min_coverage_ratio": 0.4,      # 実行を許可する最低カバー率 (40%)
             "max_seed_count": 5,            # 保存するSeedの最大数
